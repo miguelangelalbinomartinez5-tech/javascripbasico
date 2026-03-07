c20781a9ece5dc499629a0432647902f4de469ca
miguel albino <miguelangelalbinomartinez5@gmail.com>
2026-03-06 21:30:02 -0500

    feat ejercicio 10 serie2 solucionado

--- src/ejercicios2.js
+++ src/ejercicios2.js
@@ -181,6 +181,8 @@ function calcularPropina(total, porcentaje) {
 	// Ejemplo: calcularPropina(100, 15) → 15.00
 	// Pista: Usa toFixed(2) y luego convierte a número si es necesario.
 	// Tu código:
+	const propina = (total * porcentaje) / 100;
+	return Number(propina.toFixed(2));
 }
 
 // ██████  STRINGS Y VALIDACIONES ████████████████████████
